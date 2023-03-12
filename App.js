edf062c2c5dadce31bbab31d476104eb2b5f7f76
Kerde123 <kerttu.vanhala@gmail.com>
2023-03-12 17:39:44 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import * as SMS from 'expo-sms';
 
 export default function App() {
+  const [Contact, setContact] = useState({});
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers]
+      })
+
+      if (data.length > 0) {
+        setContact(data);
+      }
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.container} >
+      <Button title="Get contacts" onPress={getContacts} />
+      <View>
+        <FlatList
+          data={Contact}
+          renderItem={({ item }) =>
+          <Text>{item.name} {item.phoneNumbers[0].number}</Text>
+      } 
+      />
+      </View>
+      </View>
   );
 }
 
@@ -16,5 +41,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100,
   },
 });
